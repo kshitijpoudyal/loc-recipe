2bfcf854abc8429318c32162d8c26ccef7be0dde
Kshitij Poudyal <kshitijpoudyal@gmail.com>
2024-12-23 17:39:01 -0500

    add dom loaded state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,30 @@
 "use client";
 import AddRecipe from "@/app/pages/addRecipe/AddRecipe";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import RecipeList from "@/app/pages/listRecipe/RecipeList";
 // import saveWeeklyScheduleMock from "@/app/mockData/saveWeeklyScheduleMock";
 
 export default function Home() {
+
+    const [domLoaded, setDomLoaded] = useState(false);
+
+    useEffect(() => {
+        setDomLoaded(true);
+    }, []);
+
     return (
-        <div className="bg-white">
-            <main>
-                {/*<ViewWeeklySchedule></ViewWeeklySchedule>*/}
-                <AddRecipe></AddRecipe>
-                <RecipeList></RecipeList>
-            </main>
-        </div>
+        <>
+            {domLoaded && (
+                <div className="bg-white">
+                    <main>
+                        {/*<ViewWeeklySchedule></ViewWeeklySchedule>*/}
+                        <AddRecipe></AddRecipe>
+                        <RecipeList></RecipeList>
+                    </main>
+                </div>
+            )
+            }
+        </>
     );
 }
 
