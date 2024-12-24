40e3a8d680c1b075941e3b9b79f90d5d8f69b06b
Kshitij Poudyal <kshitijpoudyal@gmail.com>
2024-12-24 14:53:39 -0500

    create new add recipe form; WIP ingredients

--- src/app/data/DailySchedule.ts
+++ src/app/data/DailySchedule.ts
@@ -7,3 +7,13 @@ export const daysOfWeek: string[] = [
     "saturday",
     "sunday",
 ];
+
+export const weekDays = [
+    {id: 0, name: 'Monday', value: "monday"},
+    {id: 1, name: 'Tuesday', value: "tuesday"},
+    {id: 2, name: 'Wednesday', value: "wednesday"},
+    {id: 3, name: 'Thursday', value: "thursday"},
+    {id: 4, name: 'Friday', value: "friday"},
+    {id: 5, name: 'Saturday', value: "saturday"},
+    {id: 6, name: 'Sunday', value: "sunday"},
+]
