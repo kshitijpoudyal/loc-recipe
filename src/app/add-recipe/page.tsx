d03ef0ba96d9e82e3cad23d6e6cf569814f99ffd
Kshitij Poudyal <kshitijpoudyal@gmail.com>
2024-12-24 20:12:21 -0500

    Add Recipe: Clean background

--- src/app/add-recipe/page.tsx
+++ src/app/add-recipe/page.tsx
@@ -1,30 +1,18 @@
-"use client";
-import React, {useEffect, useState} from 'react';
-import {addRecipe, Recipe} from "@/app/data/Recipe";
 import AddRecipe from "@/app/components/AddRecipe";
 import NavigationMenu from "@/app/components/NavigationMenu";
-
+import React from "react";
 
 export default function AddRecipePage() {
 
-    const [domLoaded, setDomLoaded] = useState(false);
-
-    useEffect(() => {
-        setDomLoaded(true);
-    }, []);
-
-
     return (
         <>
-            {domLoaded && (
-                <div>
-                    <NavigationMenu></NavigationMenu>
-                    <main>
-                        <AddRecipe/>
-                    </main>
-                </div>
+            <div>
+                <NavigationMenu></NavigationMenu>
+                <main>
+                    <AddRecipe/>
+                </main>
+            </div>
             )
-            }
         </>
     );
 }
