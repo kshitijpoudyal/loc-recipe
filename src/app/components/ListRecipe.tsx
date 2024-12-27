3a6cca3f6cb19c79b9cd4a6df474d24d2165526d
Kshitij Poudyal <kshitijpoudyal@gmail.com>
2024-12-27 02:49:38 -0500

    reuse recipe card

--- src/app/components/ListRecipe.tsx
+++ src/app/components/ListRecipe.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import {DEFAULT_RECIPE} from "@/app/data/ConstData";
 import {Recipe} from "@/app/data/DataInterface";
 import {RecipeDetails} from "@/app/components/RecipeDetails";
+import RecipeCard from "@/app/components/RecipeCard";
 
 export default function ListRecipeComponent() {
     const [recipes, setRecipes] = useState<Recipe[]>([]);
@@ -37,36 +38,10 @@ export default function ListRecipeComponent() {
                     className="mt-11 grid grid-cols-1 items-start gap-x-6 gap-y-16 sm:mt-16 sm:grid-cols-2 lg:grid-cols-4 lg:gap-x-8">
                     {recipes.map((recipe) => (
                         <div key={recipe.recipeId} className="flex flex-col-reverse" onClick={() => {
-                            console.log("clicked", open, selectedRecipe)
                             setOpen(!open)
                             setSelectedRecipe(recipe)
                         }}>
-                            <div className="mt-6">
-                                <h3 className="text-sm font-medium text-gray-900">
-                                    {recipe.name}
-                                    {recipe.mealType && recipe.mealType.length > 0 && (
-                                        <div className="flex space-x-2">
-                                            {recipe.mealType.map((type) => (
-                                                <span
-                                                    key={type}
-                                                    className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20"
-                                                >
-                                                    {type}
-                                                </span>
-                                            ))}
-                                        </div>
-                                    )}
-                                </h3>
-                            </div>
-                            {recipe.imageUrl && (
-                                <Image
-                                    alt={recipe.name}
-                                    src={recipe.imageUrl}
-                                    height={400}
-                                    width={400}
-                                    className="aspect-square w-full rounded-lg bg-gray-100 object-cover"
-                                />
-                            )}
+                            <RecipeCard recipe={recipe} isPreview={false}/>
                         </div>
                     ))}
                 </div>
