56fb755c1e06bba2738358c78a21b0896365b328
Kshitij Poudyal <kshitijpoudyal@gmail.com>
2024-12-30 23:03:07 -0500

    add login and logout

--- src/app/config/firebase.ts
+++ src/app/config/firebase.ts
@@ -6,13 +6,13 @@ import {getAuth} from 'firebase/auth';
 
 // Firebase configuration (replace with your own Firebase project config)
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+    apiKey: "AIzaSyCIQ6Fdv7mxpib9ykhCYw0Em0xdddhwWII",
+    authDomain: "loc-recipe.firebaseapp.com",
+    projectId: "loc-recipe",
+    storageBucket: "loc-recipe.firebasestorage.app",
+    messagingSenderId: "397427942472",
+    appId: "1:397427942472:web:be949bc3d231689691247d",
+    measurementId: "G-BGF2SSQLCM"
 };
 
 // Initialize Firebase
